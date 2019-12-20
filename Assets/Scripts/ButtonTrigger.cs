262c104b932ab2eb642ec084d511a475d676b165
dododb <dorian.bataille@viacesi.fr>
2019-12-20 08:55:03 +0100

    button triger

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    Button button;
+    private readonly HashSet<string> authorisedTag = new HashSet<string>()
+    {
+        "Hand"
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(authorisedTag.Contains(other.tag))
+        {
+            button.onClick.Invoke();
+        }
+    }
+}
